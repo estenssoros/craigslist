ec95495eca8c7d131fa93981db52b0b459a4ab69
estenssoros <seb.estenssoro@gmail.com>
2016-09-12 16:03:54 -0600

    finished for today, waiting for tomorrows data

--- craigslist.py
+++ craigslist.py
@@ -6,10 +6,11 @@ import mechanize
 import cookielib
 import time
 import pandas as pd
-import progressbar
+from progressbar import Percentage, Bar, ProgressBar, Counter
 import matplotlib.path as mplPath
 import numpy as np
 import folium
+import re
 
 
 def start_browser(url):
@@ -90,8 +91,9 @@ def insert_mongo(br, links):
     db = client['craigslist']
     coll = db['housing']
 
-    bar = progressbar.ProgressBar()
+    bar = ProgressBar()
     for link in bar(links):
+        time.sleep(0.5)
         try:
             br.follow_link(link)
             html = br.response().read()
@@ -99,11 +101,12 @@ def insert_mongo(br, links):
             dic.update({'_id': link.attrs[1][1]})
             coll.insert_one(dic)
         except Exception as e:
-            with open('error_log.txt','a') as f:
-                f.write(e)
+            br = start_browser('http://denver.craigslist.org/')
+            with open('error_log.txt', 'a') as f:
+                f.write(str(e) + '\n')
 
 
-def draw_map():
+def get_boundary_points():
     p1 = (39.702248, -104.987396)
     p2 = (39.691106, -104.974581)
     p3 = (39.691106, -104.959347)
@@ -114,8 +117,12 @@ def draw_map():
     p8 = (39.771182, -104.973331)
     p9 = (39.743186, -105.014430)
     p10 = (39.718450, -105.003763)
-    points = [p1, p2, p3, p5, p6, p7, p8, p9, p10, p1]
+    points = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p1]
+    return points
 
+
+def draw_map():
+    points = get_boundary_points()
     ave_lat = sum(p[0] for p in points) / len(points)
     ave_lon = sum(p[1] for p in points) / len(points)
 
@@ -128,22 +135,102 @@ def draw_map():
     my_map.save("./housing_boundaries.html")
 
 
-def make_df(links):
+def parse_attrs(attrs):
+    bed = None
+    bath = None
+    sqft = None
+    available = None
+    laundry = None
+    apartment = 'No'
+    attrs = [a.lower() for a in attrs]
+    for a in attrs:
+        if '/' in a and 'br' in a:
+            bed = re.findall('([0-9a-z]+)br', a)[0]
+            bath = re.findall('([0-9.a-z]+)ba', a)[0]
+        elif 'ft2' in a:
+            sqft = int(a.split('ft2')[0])
+        elif 'available' in a:
+            available = a
+        elif 'laundry' in a:
+            laundry = a
+        elif 'apartment' in a:
+            apartment = 'Yes'
+
+    return bed, bath, sqft, available, laundry, apartment
+
+
+def parse_info(info):
+    posted, updated = info
+    posted = parser.parse(posted.split('ed:')[1]).date()
+    if updated:
+        updated = parser.parse(updated.split('ed:')[1]).date()
+    return posted, updated
+
+
+def make_df():
+    client = MongoClient()
+    db = client['craigslist']
+    coll = db['housing']
     df = pd.DataFrame()
-    bar = progressbar.ProgressBar()
-    for link in bar(links):
-        df = df.append({'id': link.attrs[1][1]}, ignore_index=True)
-    df['link'] = 'http://denver.craigslist.org/apa/' + df['id'] + '.html'
+    for r in coll.find():
+        attrs = r['attributes']
+        bed, bath, sqft, available, laundry, apartment = parse_attrs(attrs)
+        posted, updated = parse_info(r['info'])
+        df = df.append({'id': r['_id'],
+                        'lat': r['coord'][0],
+                        'lon': r['coord'][1],
+                        'price': r['price'][0],
+                        'bed': bed,
+                        'bath': bath,
+                        'sqft': sqft,
+                        'available': available,
+                        'laundry': laundry,
+                        'apartment': apartment,
+                        'posted': posted,
+                        'updated': updated}, ignore_index=True)
+    df = df[df['lat'].notnull()]
+    df['price'] = df['price'].str.replace('$', '').astype(int)
+    cols = ['lat', 'lon']
+    for col in cols:
+        df[col] = df[col].astype(float)
+
+    print 'df constructed!'
     return df
 
+def plot_coord(df):
+    points = get_boundary_points()
+    bbPath = mplPath.Path(np.array(points))
 
-def main():
-    pass
+    df = df[df.apply(lambda x: bbPath.contains_point((x['lat'], x['lon'])), axis=1)]
+    df = df[df['apartment'] =='No']
+    df.reset_index(drop=True, inplace=True)
+    my_map = folium.Map(location=[df['lat'].mean(), df['lon'].mean()], tiles='Stamen Toner', zoom_start=13)
+
+    pbar = ProgressBar(widgets=['Plotting: ', Counter(), Bar()], maxval=len(df) + 1).start()
+    for i, r in df.iterrows():
+        popup = 'http://denver.craigslist.org/apa/{0}.html'.format(r['id'])
+        folium.RegularPolygonMarker((r['lat'], r['lon']),
+                                    popup=popup,
+                                    fill_color='#0000ff',
+                                    number_of_sides=8,
+                                    radius=4).add_to(my_map)
+
+        pbar.update(i + 1)
+    pbar.finish()
+    for i, each in enumerate(points):
+        popup = '{0} - {1}'.format(i + 1, each)
+        folium.RegularPolygonMarker(each, popup=popup, fill_color='#ff0000', number_of_sides=3, radius=4).add_to(my_map)
+    folium.PolyLine(points, color="red", weight=2.5, opacity=1).add_to(my_map)
+    my_map.save("./found.html")
+    print 'plotting done!'
 
 
-    insert_mongo(br, links)
-
-if __name__ == '__main__':
+def fetch_day():
     url = "http://denver.craigslist.org/search/apa?hasPic=1&search_distance=3&postal=80206&max_price=2000&pets_dog=1"
     br = start_browser(url)
     links = get_links(br)
+    insert_mongo(br, links)
+
+if __name__ == '__main__':
+    df = make_df()
+    plot_coord(df)
